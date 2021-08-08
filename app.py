ff6cc65dd3371df5b659eb0629bbbf455d029a13
tony-reddington <tony.reddington@gmail.com>
2021-08-08 21:14:35 +0000

    Imported utilities from werkzeug and updated the register function to check if user meets the if statement criteria and then update mongodb

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,8 +24,43 @@ def index():
     return render_template("index.html", recipes=recipes)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    Allows user to create and account, checks the if/elif statements
+    and then check's if the user has previously been registered. 
+    On successful registration, the user is redirected to index.html
+    """
+    if request.method == "POST":
+        user = mongo.db.users
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmed_password = request.form.get("confirm_password")
+        active_user = user.find_one({"username": username.lower()})
+        
+        if active_user:
+            flash("The selected username already exists", category="error")
+            return redirect(url_for("register"))
+        elif len(username) < 5:
+            flash("Username must be more than 5 x characters long", category="error")
+            return redirect(url_for("register"))
+        elif len(password) < 5:
+            flash("Password must be more than 7 x characters long", category="error")
+            return redirect(url_for("register"))
+        elif password != confirmed_password:
+            flash("The passwords entered do not match", category="error")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(password, method="sha256")
+        }
+        user.insert_one(register)
+
+        session["username"] = request.form.get("username").lower()
+        flash("You have been successfully registered", category="success")
+        return redirect(url_for("index"))
+
     return render_template("register.html")
 
 
