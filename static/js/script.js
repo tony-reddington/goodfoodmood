343484df2413a84b315a4bfea22d263624b04c2f
tony-reddington <tony.reddington@gmail.com>
2021-08-19 14:41:23 +0000

    Removing old jquery code from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,3 @@
-$(document).ready(function () {
-    // Copyright year on base.html keeping the year matching the current year //
-    const currentYear = new Date().getFullYear();
-    document.getElementById("year").innerHTML = currentYear;
-});
+// Copyright year on base.html keeping the year matching the current year //
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
